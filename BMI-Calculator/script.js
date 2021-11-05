3afd163d569328bff3fe8959d159a262b3225387
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2021-11-05 21:09:26 +0530

    added more lines of code

--- BMI-Calculator/script.js
+++ BMI-Calculator/script.js
@@ -14,23 +14,29 @@ submit.addEventListener('click', (e) => {
 function displayResult(bmiValue) {
     const result = bmiValue.toFixed(1)
     let status = ''
-    if (bmiValue <= 18.5) {
-        status = 'Underweight'
-        display.style.color = 'cyan'
-    } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
-        status = 'Normal'
-        display.style.color = 'lime'
-    } else if (bmiValue >= 25 && bmiValue <= 29.9) {
-        status = 'Overweight'
-        display.style.color = 'yellow'
-    } else if (bmiValue >= 30 && bmiValue <= 34.9) {
-        status = 'Obese'
-        display.style.color = 'orange'
-    } else if (bmiValue >= 35) {
-        status = 'Extremely Obese'
-        display.style.color = 'red'
+    if (bmiValue <= 0 || bmiValue >= 200) {
+        alert('woah... what the hell? what values did you insert?')
+        display.innerText = ''
     }
-    display.innerText = `${result} : ${status}`
+    if (bmiValue > 0 && bmiValue < 200) {
+        if (bmiValue > 0 && bmiValue <= 18.5) {
+            status = 'Underweight'
+            display.style.color = 'cyan'
+        } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+            status = 'Normal'
+            display.style.color = 'lime'
+        } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+            status = 'Overweight'
+            display.style.color = 'yellow'
+        } else if (bmiValue >= 30 && bmiValue <= 34.9) {
+            status = 'Obese'
+            display.style.color = 'orange'
+        } else if (bmiValue >= 35 && bmiValue < 200) {
+            status = 'Extremely Obese'
+            display.style.color = 'red'
+        }
+        display.innerText = `${result} : ${status}`
+    }
     weight.value = ''
     height.value = ''
 }
