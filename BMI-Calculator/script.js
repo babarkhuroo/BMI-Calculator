b6e9a97f7abd898146067f946b71da0b3174e767
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2021-11-06 18:47:37 +0530

    added some functionalities

--- BMI-Calculator/script.js
+++ BMI-Calculator/script.js
@@ -1,42 +1,61 @@
 const weight = document.querySelector('#weight')
 const height = document.querySelector('#height')
 const submit = document.querySelector('.button')
-const display = document.querySelector('#res-value')
+const gradient = document.querySelector('.gradient')
+const pointer = document.querySelector('#pointer')
+const appendCont = document.querySelector('.sec-result')
+const disRes = document.getElementsByClassName('display-res')
+const disStat = document.getElementsByClassName('display-stat')
 
 submit.addEventListener('click', (e) => {
     e.preventDefault()
-    const weightValue = weight.value
-    const heightValue = height.value / 100
-    const bmi = weightValue / Math.pow(heightValue, 2)
-    displayResult(bmi)
+    // the chunk of code i needed help with
+    if (disRes.length > 0 && disStat.length > 0) {
+        disRes[0].remove()
+        disStat[0].remove()
+    }
+
+    if (weight.value && height.value) {
+        let weightValue = weight.value
+        let heightValue = height.value / 100
+        const bmi = weightValue / Math.pow(heightValue, 2)
+        displayResult(bmi)
+    } else {
+        alert('bruh... fill those values')
+    }
+    weight.value = height.value = ''
 })
 
 function displayResult(bmiValue) {
     const result = bmiValue.toFixed(1)
-    let status = ''
+    let status = color = ''
     if (bmiValue <= 0 || bmiValue >= 200) {
         alert('woah... what the hell? what values did you insert?')
-        display.innerText = ''
-    }
-    if (bmiValue > 0 && bmiValue < 200) {
+    } else {
         if (bmiValue > 0 && bmiValue <= 18.5) {
             status = 'Underweight'
-            display.style.color = 'cyan'
+            color = 'cyan'
         } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
             status = 'Normal'
-            display.style.color = 'lime'
+            color = 'lime'
         } else if (bmiValue >= 25 && bmiValue <= 29.9) {
             status = 'Overweight'
-            display.style.color = 'yellow'
+            color = 'yellow'
         } else if (bmiValue >= 30 && bmiValue <= 34.9) {
             status = 'Obese'
-            display.style.color = 'orange'
+            color = 'orange'
         } else if (bmiValue >= 35 && bmiValue < 200) {
             status = 'Extremely Obese'
-            display.style.color = 'red'
+            color = 'red'
         }
-        display.innerText = `${result} : ${status}`
+        const displayRes = document.createElement('div')
+        displayRes.classList.add('display-res')
+        displayRes.innerText = `${result}`
+        displayRes.style.color = `${color}`
+        const displayStat = document.createElement('div')
+        displayStat.classList.add('display-stat')
+        displayStat.innerText = `${status}`
+        appendCont.appendChild(displayRes)
+        appendCont.appendChild(displayStat)
     }
-    weight.value = ''
-    height.value = ''
 }
